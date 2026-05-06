9c655cbde4f8f9f37d5c1ad54dfd90ea05e8ddc6
zhangwuji8121 <1486310817@qq.com>
2026-05-06 19:40:35 +0800

    Bump LICENSE_CONNECTIONS to 99999 for unrestricted concurrent edits

--- Common/sources/constants.js
+++ Common/sources/constants.js
@@ -88,7 +88,7 @@ exports.LICENSE_RESULT = {
   NotBefore: 16
 };
 
-exports.LICENSE_CONNECTIONS = 20;
+exports.LICENSE_CONNECTIONS = 99999;
 exports.LICENSE_USERS = 3;
 exports.LICENSE_EXPIRE_USERS_ONE_DAY = 24 * 60 * 60; // day in seconds
 
